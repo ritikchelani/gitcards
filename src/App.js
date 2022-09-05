cb7a51e0b477975f9c4d75c1c349c7d3dab5cb29
Ritik Chelani <100259409+ritikchelani@users.noreply.github.com>
2022-09-05 14:39:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     const [loading, setLoading] = useState(false)
 
     useEffect(()=>{
-        fetchUserData('MtechZilla')
+        fetchUserData('ritikchelani')
     },[])
     function handleSearch(username) {
         setLoading(true)
